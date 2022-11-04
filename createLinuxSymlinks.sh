700089209685b3864d0bd39879d184a4af63df06
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-11-04 21:19:20 +0000

    Created file to creat symbolic links for dotfiles

---
+++ createLinuxSymlinks.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Create Linux symlinks for config files
+ln -s .vimrc ~/.vimrc
+ln -s mpv.conf ~/.config/mpv/
+ln -s .inputrc ~/
+ln -s .dircolors ~/
+ln -s .xsession ~/
+ln -s .Xresources ~/
+ln -s .bashrc ~/
+
+# This installs the vim bundle manager vundle This must be done before you first run vim to install the rest of the packages
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# This applies the X resources file which applies the colours for the system 
+xrdb ~/.Xresources
